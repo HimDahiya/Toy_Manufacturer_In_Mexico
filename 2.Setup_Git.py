809dd69b552d17abef9e50e2ae85db98c7dd8a3c
Himanshu Dahiya <69037721+HimDahiya@users.noreply.github.com>
2025-03-13 03:08:28 -0400

    Add files via upload

---
+++ 2.Setup_Git.py
@@ -0,0 +1,34 @@
+# All steps need to be done in the terminal
+
+# Create a folder
+# mkdir folder_name
+
+# How to change directory
+# cd folder_name
+
+# Initialize the git in the folder created
+# git init
+
+# How to add all changes in the staging file. This will ignore the data from .gitignore
+# git add .
+
+# How to commit the changes
+# git commit -m 'first working file of project. You can add any relevant description'
+
+# How to publish to GitHub for the first time
+# git remote add origin https://github.com/tsonma/Toy_Manufacturer_Mexico.git
+
+# Look at current branch name
+# git branch
+
+# How to rename the local branh
+# git branch -M main
+
+#How to pull data from GitHub
+# git pull origin main
+
+# How to republish to GitHub
+# git push origin main
+
+# How to remove origin if you have errors
+# git remote remove origin
